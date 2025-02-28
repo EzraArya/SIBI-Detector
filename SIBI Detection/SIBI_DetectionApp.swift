ce8bcefa8b13f9e965594dc94cbfaf2fcdc9e37f
EzraArya <ezrawijaya10@gmail.com>
2025-03-01 00:31:13 +0700

    initial commit

---
+++ SIBI Detection/SIBI_DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIBI_DetectionApp.swift
+//  SIBI Detection
+//
+//  Created by Ezra Arya Wijaya on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SIBI_DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
